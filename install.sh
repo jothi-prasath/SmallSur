9f1c23e2cb9bec2d07c683e158573dec43868c65
jothi prasath <jothiprasath2@gmail.com>
2021-11-16 02:40:54 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-cp -r xpple_menu/applications/ /home/jothiprasath/.local/share
-cp xpple_menu/xpple.menu /home/jothiprasath/.config/menu/
-cp -r "ulauncher theme"/* /home/jothiprasath/.config/ulauncher/
+cp -r xpple_menu/applications/ /home/"$USER"/.local/share
+cp xpple_menu/xpple.menu /home/"$USER"/.config/menu/
+cp -r "ulauncher theme"/* /home/"$USER"/.config/ulauncher/
 
 
-cd /home/jothiprasath/Downloads/
+cd /home/"$USER"/Downloads/
 git clone https://github.com/vinceliuice/WhiteSur-gtk-theme.git
 WhiteSur-gtk-theme/install.sh -c dark -c light
 git clone https://github.com/vinceliuice/WhiteSur-icon-theme.git
 WhiteSur-icon-theme/install.sh
 git clone https://github.com/vinceliuice/WhiteSur-cursors.git
 WhiteSur-cursors/install.sh
-cp -r WhiteSur-gtk-theme/src/other/plank/* /home/jothiprasath/.local/share/plank/themes/
+cp -r WhiteSur-gtk-theme/src/other/plank/* /home/"$USER"/.local/share/plank/themes/
 
 
