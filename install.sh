b3555870b569034897f94823cd2712cc6be2c819
jothiprasath <jothiprasath2@gmail.com>
2021-12-06 16:54:50 +0530

    update

--- install.sh
+++ install.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
 cp -r xpple_menu/applications /home/"$USER"/.local/share
-mkdir /home/"$USER"/.config/menu/
+mkdir -p /home/"$USER"/.config/menu/
 cp -r xpple_menu/xpple.menu /home/"$USER"/.config/menu/
+mkdir -p /home/"$USER"/.config/ulauncher/
 cp -r "ulauncher theme"/* /home/"$USER"/.config/ulauncher/
 
 
-cd /home/"$USER"/Downloads/
 git clone https://github.com/vinceliuice/WhiteSur-gtk-theme.git
 WhiteSur-gtk-theme/install.sh -c dark -c light
 git clone https://github.com/vinceliuice/WhiteSur-icon-theme.git
 WhiteSur-icon-theme/install.sh
 git clone https://github.com/vinceliuice/WhiteSur-cursors.git
 WhiteSur-cursors/install.sh
-cp -r /home/"$USER"/Downloads/WhiteSur-gtk-theme/src/other/plank/* /home/"$USER"/.local/share/plank/themes/
+mkdir -p /home/"$USER"/.local/share/plank/themes/
+cp -r WhiteSur-gtk-theme/src/other/plank/* /home/"$USER"/.local/share/plank/themes/
+cp -r plank/mcOS-BS-iMacM1-Black/ /home/"$USER"/.local/share/plank/themes/
 
 
