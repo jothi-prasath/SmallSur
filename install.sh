4473ee9535e7ddabc4baa088356a50aaebb1ff50
jothi prasath <jothiprasath2@gmail.com>
2022-03-13 21:34:23 +0530

    fixed ulauncher permission

--- install.sh
+++ install.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-user_dir="$SUDO_USER"
+user_name="$SUDO_USER"
 
 #Global menu
 apt install xfce4-appmenu-plugin
 
 #Xpple menu
-mkdir -p /home/"$user_dir"/.local/share/
-cp -r xpple_menu/applications /home/"$user_dir"/.local/share
-mkdir -p /home/"$user_dir"/.config/menu/
-cp -r xpple_menu/xpple.menu /home/"$user_dir"/.config/menu/
+mkdir -p /home/"$user_name"/.local/share/
+cp -r xpple_menu/applications /home/"$user_name"/.local/share
+mkdir -p /home/"$user_name"/.config/menu/
+cp -r xpple_menu/xpple.menu /home/"$user_name"/.config/menu/
 
 #GTK theme
 git clone https://github.com/vinceliuice/WhiteSur-gtk-theme.git
@@ -21,14 +21,15 @@ WhiteSur-icon-theme/install.sh
 
 #Cursors
 git clone https://github.com/vinceliuice/WhiteSur-cursors.git
-cp -r WhiteSur-cursors/dist/ /home/$user_dir/.local/share/icons/
+cp -r WhiteSur-cursors/dist/ /home/$user_name/.local/share/icons/
 
 #Ulaucher theme
-mkdir -p /home/"$user_dir"/.config/ulauncher/
-cp -r "ulauncher theme"/* /home/"$user_dir"/.config/ulauncher/
+mkdir -p /home/"$user_name"/.config/ulauncher/
+cp -r "ulauncher theme"/* /home/"$user_name"/.config/ulauncher/
+chown -R $user_name /home/"$user_name"/.config/ulauncher/*
 
 #Plank themes
-mkdir -p /home/"$user_dir"/.local/share/plank/themes/
-cp -r WhiteSur-gtk-theme/src/other/plank/* /home/"$user_dir"/.local/share/plank/themes/
-cp -r plank/mcOS-BS-iMacM1-Black/ /home/"$user_dir"/.local/share/plank/themes/
+mkdir -p /home/"$user_name"/.local/share/plank/themes/
+cp -r WhiteSur-gtk-theme/src/other/plank/* /home/"$user_name"/.local/share/plank/themes/
+cp -r plank/mcOS-BS-iMacM1-Black/ /home/"$user_name"/.local/share/plank/themes/
 
