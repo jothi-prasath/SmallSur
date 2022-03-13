b406cadc6c03a045b514be63abd5ff42c32f26f3
jothi prasath <jothiprasath2@gmail.com>
2022-03-13 18:37:06 +0530

    added global menu

--- install.sh
+++ install.sh
@@ -1,24 +1,34 @@
 #!/bin/bash
 
-cp -r xpple_menu/applications /home/"$USER"/.local/share
-mkdir -p /home/"$USER"/.config/menu/
-cp -r xpple_menu/xpple.menu /home/"$USER"/.config/menu/
+user_dir="$SUDO_USER"
+
+#Global menu
+apt install xfce4-appmenu-plugin
+
+#Xpple menu
+mkdir -p /home/"$user_dir"/.local/share/
+cp -r xpple_menu/applications /home/"$user_dir"/.local/share
+mkdir -p /home/"$user_dir"/.config/menu/
+cp -r xpple_menu/xpple.menu /home/"$user_dir"/.config/menu/
 
 #GTK theme
 git clone https://github.com/vinceliuice/WhiteSur-gtk-theme.git
 WhiteSur-gtk-theme/install.sh -c dark -c light
+
 #Icons
 git clone https://github.com/vinceliuice/WhiteSur-icon-theme.git
 WhiteSur-icon-theme/install.sh
+
 #Cursors
 git clone https://github.com/vinceliuice/WhiteSur-cursors.git
-WhiteSur-cursors/install.sh
-#Ulaucher theme
-mkdir -p /home/"$USER"/.config/ulauncher/
-cp -r "ulauncher theme"/* /home/"$USER"/.config/ulauncher/
-#Plank theme
-mkdir -p /home/"$USER"/.local/share/plank/themes/
-cp -r WhiteSur-gtk-theme/src/other/plank/* /home/"$USER"/.local/share/plank/themes/
-cp -r plank/mcOS-BS-iMacM1-Black/ /home/"$USER"/.local/share/plank/themes/
+cp -r WhiteSur-cursors/dist/ /home/$user_dir/.local/share/icons/
 
+#Ulaucher theme
+mkdir -p /home/"$user_dir"/.config/ulauncher/
+cp -r "ulauncher theme"/* /home/"$user_dir"/.config/ulauncher/
+
+#Plank themes
+mkdir -p /home/"$user_dir"/.local/share/plank/themes/
+cp -r WhiteSur-gtk-theme/src/other/plank/* /home/"$user_dir"/.local/share/plank/themes/
+cp -r plank/mcOS-BS-iMacM1-Black/ /home/"$user_dir"/.local/share/plank/themes/
 
